5982bfe0cf6fb13c2c6730b4953e6b5249c4b845
John da Eira <jdaeira@gmail.com>
2018-09-21 08:22:51 -0700

    Update 230-add-filemanager-actions.sh

--- Install-Scripts/230-add-filemanager-actions.sh
+++ Install-Scripts/230-add-filemanager-actions.sh
@@ -11,3 +11,9 @@ sudo apt-get install filemanager-actions-nautilus-extension -y
 sudo apt install xdotool -y
 
 echo "########### Install Filemanager Actions #############"
+
+
+#All File Manager Extensions
+#sudo apt-get install filemanager-actions-nautilus-extension # Nautilus
+#sudo apt-get install filemanager-actions-caja-extension # Caja
+#sudo apt-get install filemanager-actions-nemo-extension # Nemo
