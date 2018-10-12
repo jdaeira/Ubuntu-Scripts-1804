0c02981f1fdbb98efd953152d230325c5447b3a6
John da Eira <jdaeira@gmail.com>
2018-10-12 10:17:04 -0700

    Added a Conky Folder for installation

---
+++ Conky Install/260-install-conky.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+set -e
+########################################################################
+# Author    : John da Eira
+# Email     : jdaeira@gmail.com
+######################################################################## 
+
+ 
+sudo apt install conky-all -y
+sudo apt install libgee-0.8-2
+sudo dpkg -i realpath_8.26-3ubuntu4_all.deb
+sudo dpkg -i conky-manager_2.4~136~ubuntu16.04.1_amd64.deb
+
+
+echo "############             Conky Installed               ############"
